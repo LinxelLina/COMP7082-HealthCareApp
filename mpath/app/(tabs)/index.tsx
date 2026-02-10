82525dd4841d488b334df36561440ff942635c67
Lina <109928286+LinxelLina@users.noreply.github.com>
2026-02-09 23:55:47 -0800

    Changed to light mode, no longer on automatic. Tested lists

--- mpath/app/(tabs)/index.tsx
+++ mpath/app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import { ThemedView } from '@/components/themed-view';
 // Controls the appearance of the users status bar
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, Pressable, Text, View } from "react-native";
+import { Button, Pressable, Text, View, StyleSheet } from "react-native";
 // Keeps the app from overlapping with statusbar
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, Tabs, Link, router } from 'expo-router';
@@ -118,21 +118,22 @@ export default function HomeScreen() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
--- mpath/app/temp_list.tsx
+++ mpath/app/temp_list.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { View, Text, TextInput, StyleSheet, FlatList, Button } from "react-native";
 
     const temp_list: string[]=
     [
@@ -8,9 +9,17 @@ import { View, Text, TextInput } from "react-native";
         "Feed the fish"
     ]
 
+    type TempListProps = {
+        category: string;
+        goal: string;
+    };
+
 export default function Temp_List() {
     const [currentList, setCurrentList] = useState<string[]>([]);
     const [addGoal, setAddGoal] = useState("");
+
+    const [selectedCategory, setSelectedCategory] = useState("Food/Drink");
+    const [items, setItems] = useState<TempListProps[]>([]);
 
     useEffect(()=>{
         console.log("Data loaded");
@@ -32,27 +41,106 @@ export default function Temp_List() {
         setCurrentList(prevList =>[...prevList, addGoal]);
     }
 
+    const addItem = () => {
+    setItems(prev => [
+        ...prev,
+        {
+        category: selectedCategory,
+        goal: addGoal,
+        },
+    ]);
+    };
+
+    
     return (
     <>
     <View>
-        {currentList?.map((point, index)=>(
+        <FlatList
+            data={currentList}
+            keyExtractor={(item) => item}
+            renderItem={({ item, index }) => (
+                <Text key={index} style={styles.item}>{item}</Text>
+            )}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+        />
+
+        {/* {currentList?.map((point, index)=>(
             <Text   
             key = {index}
-            style={{
-                color: "green",
-                fontSize: 32,
-                fontWeight: "bold",
-                }}>
+            // style={styles{
+            //     color: "green",
+            //     fontSize: 32,
+            //     fontWeight: "bold",
+                // }}
+                >
                 {point}
             </Text>
-            ))}
+            ))} */}
 
         <TextInput placeholder="Enter Goal"
             value={addGoal}
             onChangeText={setAddGoal}
             onSubmitEditing={addToList}
             returnKeyType="done"/>
+
+              {/* Select Menu */}
+            <Picker
+                selectedValue={selectedCategory}
+                onValueChange={(value) => setSelectedCategory(value)}
+                style={styles.picker}
+            >
+                <Picker.Item label="Fruit" value="Fruit" />
+                <Picker.Item label="Vegetables" value="Vegetables" />
+                <Picker.Item label="Dairy" value="Dairy" />
+                <Picker.Item label="Meat" value="Meat" />
+            </Picker>
+
+            <Button title="Add to list" onPress={addItem} />
+
+            {/* List */}
+            <FlatList
+                data={items}
+                keyExtractor={(item) => item.goal}
+                renderItem={({ item }) => (
+                <View style={styles.item2}>
+                    <Text>{item.goal}</Text>
+                </View>
+                )}
+                ItemSeparatorComponent={() => <View style={styles.separator2} />}
+                style={styles.list}
+            />
     </View>
     </>
     );
+
+    
 }
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#ccc",
+    marginHorizontal: 16,
+  },
+    container: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  picker: {
+    marginHorizontal: 16,
+  },
+  list: {
+    marginTop: 20,
+  },
+  item2: {
+    padding: 16,
+  },
+  separator2: {
+    height: 1,
+    backgroundColor: "#ddd",
+    marginHorizontal: 16,
+  },
+});
