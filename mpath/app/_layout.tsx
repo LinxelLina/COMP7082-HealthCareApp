2d8fa9bfc7dfb308ace3f4aae2e5fa21fed0ceb9
scotthops <scottyhops@gmail.com>
2026-04-08 18:46:33 -0700

    refactoring goal_details

--- mpath/app/_layout.tsx
+++ mpath/app/_layout.tsx
@@ -32,11 +32,7 @@ export default function RootLayout() {
       <Stack screenOptions={{headerBackTitle: "Back"}}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        {/* <Stack.Screen name="index" options={{title: "Home"}} /> */}
-        {/* <Stack.Screen name="profile" options={{title: "Profile"}} /> */}
-        {/* <Stack.Screen name="goal_page" options={{title: "Goal Page"}} /> */}
         <Stack.Screen name="charity_form" options={{title: "Charity Form"}} />
-        {/* <Stack.Screen name="charity_list" options={{title: "Charity List"}} /> */}
         <Stack.Screen name="charity_graph" options={{title: "Charity Graph"}}/>
         <Stack.Screen name="goal_form" options={{title: "Goal Form"}} />
         <Stack.Screen name="goal_detail" options={{ title: "Goal Detail" }} />
